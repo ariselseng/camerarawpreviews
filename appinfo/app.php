f347efce34f30ca87b2e3e8d184cca22a56f4402
Ari Selseng <ari@selseng.net>
2018-04-02 12:32:42 +0200

    Throw error if dependencies are not met.

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,17 +1,19 @@
 <?php
 
 use OCP\AppFramework\App;
-use \OCP\Files\FileInfo;
-
-
-$app = new App('camerarawpreviews');
+$app       = new App('camerarawpreviews');
 $container = $app->getContainer();
-$mimeTypeDetector = \OC::$server->getMimeTypeDetector();
-$mimeTypeLoader = \OC::$server->getMimeTypeLoader();
-// Register custom mimetype we can hook in the frontend
-$mimeTypeDetector->getAllMappings();
-$mimeTypeDetector->registerType('indd', 'image/x-indesign', 'application/x-indesign');
+if (extension_loaded('imagick') && !empty(\OC_Helper::findBinaryPath('exiftool'))) {
+    $mimeTypeDetector = \OC::$server->getMimeTypeDetector();
+    $mimeTypeLoader   = \OC::$server->getMimeTypeLoader();
+    // Register custom mimetype we can hook in the frontend
+    $mimeTypeDetector->getAllMappings();
+    $mimeTypeDetector->registerType('indd', 'image/x-indesign', 'application/x-indesign');
 
-$previewManager = $container->getServer()->query('PreviewManager');
-$previewManager->registerProvider('/image\/x-dcraw/', function() { return new \OCA\CameraRawPreviews\RawPreview; });
-$previewManager->registerProvider('/image\/x-indesign/', function() { return new \OCA\CameraRawPreviews\IndesignPreview; });
+    $previewManager = $container->getServer()->query('PreviewManager');
+
+    $previewManager->registerProvider('/image\/x-dcraw/', function () {return new \OCA\CameraRawPreviews\RawPreview;});
+    $previewManager->registerProvider('/image\/x-indesign/', function () {return new \OCA\CameraRawPreviews\IndesignPreview;});
+} else {
+    \OCP\Util::writeLog('core', 'Camera Raw Previews: Needs imagick and exiftool.', \OCP\Util::ERROR);
+}
