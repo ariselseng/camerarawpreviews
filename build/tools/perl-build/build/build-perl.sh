84f71527cd1af4eb5c2dbda72d41b9fac6b25468
Ari Selseng <ari@selseng.net>
2020-08-03 17:41:18 +0200

    Remove upx compression as it leads to false positives in certain AVs

--- build/tools/perl-build/build/build-perl.sh
+++ build/tools/perl-build/build/build-perl.sh
@@ -6,6 +6,5 @@
 apk -U add upx curl wget alpine-sdk &&
  curl http://cvs.schmorp.de/App-Staticperl/bin/staticperl -o ./staticperl && chmod +x ./staticperl && \
  ./staticperl mkapp exiftool.bin --boot exiftool_wrapper.pl perl.bundle && \
-upx exiftool.bin
 
 [ -e ./staticperl ] && rm ./staticperl
